e983f269c56ba0676f559020ed3153dfccb221cb
Enric Morales <me@enric.me>
2012-10-15 15:09:47 +0200

    add Cmus* remote control script

---
+++ remote.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+outputNowPlaying() {
+	notify-send -t 2000 -- "Now playing:" "\"$(echo $TITLE)\"\nby $(echo $ARTIST)"
+}
+
+parseOutput() {
+	grep " $1 " $TMP | cut -d " "  -f 3-
+}
+
+getStatus() {
+	TMP=$(mktemp)
+	cmus-remote -Q> $TMP
+	ARTIST="$(parseOutput artist)"
+	ALBUM="$(parseOutput album)"
+	TITLE="$(parseOutput title)"
+	rm $TMP
+}
+
+case $1 in
+	togglePause)
+		getStatus
+		cmus-remote --pause
+		outputNowPlaying
+		;;
+	next)
+		getStatus
+		cmus-remote --next
+		outputNowPlaying
+		;;
+	prev)
+		getStatus
+		cmus-remote --prev
+		outputNowPlaying
+		;;
+	stop)
+		cmus-remote --stop
+		;;
+esac
