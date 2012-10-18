e00411c1f4ad5d7214985a9e08363853bda27f3f
Enric Morales <me@enric.me>
2012-10-18 14:51:44 +0200

    add song udraiton output

--- remote.sh
+++ remote.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
+formatTime() {
+	local S=${1}
+	((m=S%3600/60))
+	((s=S%60))
+	[ ${#s} == 1 ] && s=0$s	# Add a leading zero if needed
+
+	echo $m:$s
+}
+
 outputNowPlaying() {
-	notify-send -t 2000 -- "Now playing:" "\"$(echo $TITLE)\"\nby $(echo $ARTIST)"
+	if [ -n "$TITLE" ] && [ -n "DURATION" ]
+	then
+		notify-send -t 1000 -- "Now playing" "\"$TITLE\" ($DURATION)\nby $ARTIST"
+	else
+		notify-send -t 1000 -- "Nothing playing"
+	fi
 }
 
 parseOutput() {
-	grep " $1 " $TMP | cut -d " "  -f 3-
+	grep -w $1 $TMP | cut -d " "  -f 3-
 }
 
 getStatus() {
@@ -14,23 +28,24 @@ getStatus() {
 	ARTIST="$(parseOutput artist)"
 	ALBUM="$(parseOutput album)"
 	TITLE="$(parseOutput title)"
+	DURATION="$(formatTime $(grep -w duration $TMP | cut -d " " -f 2))"
 	rm $TMP
 }
 
 case $1 in
 	togglePause)
-		getStatus
 		cmus-remote --pause
+		getStatus
 		outputNowPlaying
 		;;
 	next)
-		getStatus
 		cmus-remote --next
+		getStatus
 		outputNowPlaying
 		;;
 	prev)
-		getStatus
 		cmus-remote --prev
+		getStatus
 		outputNowPlaying
 		;;
 	stop)
