23c8d940b4a21d1c225e43a9e4726f619ea0fae9
Enric Morales <me@enric.me>
2012-11-24 15:06:27 +0100

    any2pdf, a pandoc wrapper

--- bat.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-# Outputs the battery level of an {i,Power}Book G4.
-
-cat /proc/pmu/battery_0 | tr -d ' .' | tr ':' '=' > /tmp/battery_0
-source /tmp/battery_0
-echo $((100 * $charge / $max_charge))"%"
--- sys.sh
+++ sys.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+
 arch=$(uname -m)
+
 if [ "$arch" == "ppc" ] || [ "$arch" == "ppc64" ]; then
 	cat /proc/pmu/battery_0 | tr -d ' .' | tr ':' '=' > /tmp/battery_0
 	source /tmp/battery_0
@@ -14,6 +16,7 @@ elif [ "$arch" == "x86" ] || [ "$arch" == "x86_64" ]; then
 	cur_charge=$(cat /sys/class/power_supply/BAT0/energy_now)
 	bat="$((100 * $cur_charge / $max_charge))%"
 	T="/sys/class/thermal/"
+	fan="$(cat $T/sensor1_fan_speed | cut -f2-3 -d' ' | tr -d ' ()')"
 	cpu_temp_full=$(cat $T/thermal_zone0/temp)
 	cpu_temp=${cpu_temp_full%000}
 fi
