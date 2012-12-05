744cfa9bbdebb6646f230840dbac4f0a2accc7f6
Enric Morales <me@enric.me>
2012-12-05 22:46:48 +0100

    edid sys.sh for Raspberry Pi

--- sys.sh
+++ sys.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 arch=$(uname -m)
-if [ "$arch" == "ppc" ] || [ "$arch" == "ppc64" ]; then
+if [ "$arch" == "armv6l" ]; then
+	T="$(cat /sys/class/thermal/thermal_zone0/temp)"
+	cpu_temp=${T::-3}
+
+elif [ "$arch" == "ppc" ] || [ "$arch" == "ppc64" ]; then
 	cat /proc/pmu/battery_0 | tr -d ' .' | tr ':' '=' > /tmp/battery_0
 	source /tmp/battery_0
 	T="/sys/devices/temperatures/"
 	fan="$(cat $T/sensor1_fan_speed | cut -f2-3 -d' ' | tr -d ' ()')"
 	cpu_temp="$(cat $T/sensor1_temperature)ºC"
 	gpu_temp="$(cat $T/sensor2_temperature)ºC"
-	bat="$((100 * $charge / $max_charge))%"
+	bat="$((100 * $charge / $max_charge))"
 	
 elif [ "$arch" == "x86" ] || [ "$arch" == "x86_64" ]; then
 	max_charge=$(cat /sys/class/power_supply/BAT0/energy_full)
@@ -18,4 +22,6 @@ elif [ "$arch" == "x86" ] || [ "$arch" == "x86_64" ]; then
 	cpu_temp=${cpu_temp_full%000}
 fi
 	
-echo "f:$fan | t:$cpu_temp | ↯$bat"
+test -z $fan || echo -n "f:$fan | " 
+test -z $cpu_temp || echo -n "t:${cpu_temp}C | "
+test -z $bat || echo -n "↯${bat}% | "
